f67de94df1ff0125204125e9cb39f8c80ee0165a
daedric7 <ricardo.dra.vieira@gmail.com>
2024-09-19 12:26:05 +0100

    Update entrypoint.sh
    Change to upload a file and send a message with it.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,27 +1,21 @@
 #!/bin/sh
 
-# structure_message $content $formatted_content (optional)
-structure_message() {
-  subject="$2"
-  if [ -z "$subject" ]; then
-      subject="$1"
-  fi
-  parsed="$(echo "$1" | markdown)"
-  body=$(jq -Rs --arg body "$subject" --arg formatted_body "$parsed" '{"msgtype": "m.text", $body, "format": "org.matrix.custom.html", $formatted_body}' < /dev/null)
-  echo "$body"
+escape() {
+        local multil=
+        [ $(echo "$1" | wc -l) -gt 1 ] && multil="-s"
+        jq $multil -R . <<<"$1"
 }
 
-# send_message $body (json formatted) $room_id $access_token
-send_message() {
-  out="$(curl -XPOST -d "$1" "https://$INPUT_SERVER/_matrix/client/r0/rooms/$2/send/m.room.message?access_token=$3")"
-  if [ "$(echo "$out" | jq .errcode)" = "null" ]; then
-    echo "[+] Message sent!"
-  else
-    echo '[!] Something went wrong sending the message'
-    echo "$out" | jq '.error'
-    exit 1
-  fi
-}
-echo "Subject: $INPUT_SUBJECT"
-echo "Message: $INPUT_MESSAGE"
-send_message "$(structure_message "$INPUT_MESSAGE" "$INPUT_SUBJECT")" "$INPUT_ROOM_ID" "$INPUT_ACCESS_TOKEN"
+echo "File: $FILE"
+
+content_type=$(file --brief --mime-type "$FILE")
+
+response=$(curl -s -H "$AUTHORIZATION" -H "User-Agent: github/send-file" -XPOST --data-binary "$FILE" -H "Content-Type: $content_type" "$INPUT_SERVER/_matrix/media/r0/upload?filename=$FILE" )
+
+uri=$(jq -r .content_uri <<<"$response")
+
+data="{\"body\":$(escape "$filename"), \"msgtype\":\"m.file\", \"filename\":$(escape "$filename"), \"url\":\"$uri\"}"
+
+txn=`date +%s%N`
+
+response=$( _curl -XPUT -H "Content-Type: application/json" --data "$data" "$INPUT_SERVER"/_matrix/client/v3/rooms/$INPUT_ROOM_ID/send/m.room.message/$txn"" )
