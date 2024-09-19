2378d63e166d6bcd8f48b90a62345e607ac93f62
daedric7 <ricardo.dra.vieira@gmail.com>
2024-09-19 14:53:31 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,20 +6,21 @@ escape() {
         jq $multil -R . <<<"$1"
 }
 
+filename=$(basename "$INPUT_FILE")
+
 echo "File: $INPUT_FILE"
 echo "Server: $INPUT_SERVER"
 echo "Token: $INPUT_ACCESS_TOKEN"
 echo "Room: $INPUT_ROOM_ID"
+echo "Filename: $filename"
 
 content_type=$(file --brief --mime-type "$INPUT_FILE")
 
-response=$(curl -s --oauth2-bearer "$INPUT_ACCESS_TOKEN" -H "User-Agent: github/send-file" -XPOST --data-binary "$INPUT_FILE" -H "Content-Type: $content_type" "$INPUT_SERVER/_matrix/media/r0/upload?filename=$INPUT_FILE" )
+response=$(curl -s --oauth2-bearer "$INPUT_ACCESS_TOKEN" -H "User-Agent: github/send-file" -XPOST --data-binary "$INPUT_FILE" -H "Content-Type: $content_type" "$INPUT_SERVER/_matrix/media/r0/upload?filename=$filename" )
 
 echo $response
 
 uri=$(jq -r .content_uri <<<"$response")
-
-filename=$(basename "$INPUT_FILE")
 
 data="{\"body\":$(escape "$filename"), \"msgtype\":\"m.file\", \"filename\":$(escape "$filename"), \"url\":\"$uri\"}"
 
