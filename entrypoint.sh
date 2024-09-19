4c7345aa59a5dcfd5f6f4933ba434f1b40e3b9ac
daedric7 <ricardo.dra.vieira@gmail.com>
2024-09-19 20:31:13 +0100

    Update entrypoint.sh
    Send a message before sending the file.

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,8 @@ echo "File: $INPUT_FILE"
 echo "Server: $INPUT_SERVER"
 echo "Token: $INPUT_ACCESS_TOKEN"
 echo "Room: $INPUT_ROOM_ID"
+echo "Message: $INPUT_MESSAGE"
 
+
+/matrix.sh --token=$INPUT_ACCESS_TOKEN --homeserver=$INPUT_SERVER --room=$INPUT_ROOM_ID $INPUT_MESSAGE
 /matrix.sh --token=$INPUT_ACCESS_TOKEN --homeserver=$INPUT_SERVER --room=$INPUT_ROOM_ID --file=$INPUT_FILE
