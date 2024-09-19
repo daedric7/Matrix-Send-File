3d2b35321897f02f4d22d08066daa0f4fcb35243
daedric7 <ricardo.dra.vieira@gmail.com>
2024-09-19 15:19:29 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -16,7 +16,7 @@ echo "Filename: $filename"
 
 content_type=$(file --brief --mime-type "$INPUT_FILE")
 
-response=$(curl -s --oauth2-bearer "$INPUT_ACCESS_TOKEN" -H "User-Agent: github/send-file" -XPOST --data-binary "$INPUT_FILE" -H "Content-Type: $content_type" "$INPUT_SERVER/_matrix/media/r0/upload?filename=$filename" )
+response=$(curl -s --oauth2-bearer "$INPUT_ACCESS_TOKEN" -H "User-Agent: github/send-file" -XPOST --data-binary "@$INPUT_FILE" -H "Content-Type: $content_type" "$INPUT_SERVER/_matrix/media/r0/upload?filename=$filename" )
 
 echo $response
 
