a50923155ca09f98fec5fa2f2d6457d746fe9d0e
daedric7 <ricardo.dra.vieira@gmail.com>
2024-09-19 13:47:47 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,6 +12,8 @@ content_type=$(file --brief --mime-type "$INPUT_FILE")
 
 response=$(curl -s --oauth2-bearer "$INPUT_ACCESS_TOKEN" -H "User-Agent: github/send-file" -XPOST --data-binary "$INPUT_FILE" -H "Content-Type: $content_type" "$INPUT_SERVER/_matrix/media/r0/upload?filename=$INPUT_FILE" )
 
+echo $response
+
 uri=$(jq -r .content_uri <<<"$response")
 
 filename=$(basename "$INPUT_FILE")
@@ -21,3 +23,5 @@ data="{\"body\":$(escape "$filename"), \"msgtype\":\"m.file\", \"filename\":$(es
 txn=`date +%s%N`
 
 response=$( curl -s --oauth2-bearer "$INPUT_ACCESS_TOKEN" -H "User-Agent: github/send-file" -XPUT -H "Content-Type: application/json" --data "$data" "$INPUT_SERVER"/_matrix/client/v3/rooms/$INPUT_ROOM_ID/send/m.room.message/$txn"" )
+
+echo $response
