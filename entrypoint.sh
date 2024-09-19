1de85df8aba738a9e32eb4199137820bde899bda
daedric7 <ricardo.dra.vieira@gmail.com>
2024-09-19 12:59:02 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,16 +6,18 @@ escape() {
         jq $multil -R . <<<"$1"
 }
 
-echo "File: $FILE"
+echo "File: $INPUT_FILE"
 
-content_type=$(file --brief --mime-type "$FILE")
+content_type=$(file --brief --mime-type "$INPUT_FILE")
 
-response=$(curl -s -H "$AUTHORIZATION" -H "User-Agent: github/send-file" -XPOST --data-binary "$FILE" -H "Content-Type: $content_type" "$INPUT_SERVER/_matrix/media/r0/upload?filename=$FILE" )
+response=$(curl -s --oauth2-bearer "$INPUT_ACCESS_TOKEN" -H "User-Agent: github/send-file" -XPOST --data-binary "$INPUT_FILE" -H "Content-Type: $content_type" "$INPUT_SERVER/_matrix/media/r0/upload?filename=$INPUT_FILE" )
 
 uri=$(jq -r .content_uri <<<"$response")
+
+filename=$(basename "$INPUT_FILE")
 
 data="{\"body\":$(escape "$filename"), \"msgtype\":\"m.file\", \"filename\":$(escape "$filename"), \"url\":\"$uri\"}"
 
 txn=`date +%s%N`
 
-response=$( _curl -XPUT -H "Content-Type: application/json" --data "$data" "$INPUT_SERVER"/_matrix/client/v3/rooms/$INPUT_ROOM_ID/send/m.room.message/$txn"" )
+response=$( curl -s --oauth2-bearer "$INPUT_ACCESS_TOKEN" -H "User-Agent: github/send-file" -XPUT -H "Content-Type: application/json" --data "$data" "$INPUT_SERVER"/_matrix/client/v3/rooms/$INPUT_ROOM_ID/send/m.room.message/$txn"" )
