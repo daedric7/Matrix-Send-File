13687c59b0649d7d0c2977bdd847f65cf4c7c854
daedric7 <ricardo.dra.vieira@gmail.com>
2024-11-11 21:14:46 +0000

    Add optional filename rename on sending

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ echo "Server: $INPUT_SERVER"
 echo "Token: $INPUT_ACCESS_TOKEN"
 echo "Room: $INPUT_ROOM_ID"
 echo "Message: $INPUT_MESSAGE"
-
+echo "Filename: $FILENAME"
 
 /matrix.sh --token=$INPUT_ACCESS_TOKEN --homeserver=$INPUT_SERVER --room=$INPUT_ROOM_ID "$INPUT_MESSAGE"
-/matrix.sh --token=$INPUT_ACCESS_TOKEN --homeserver=$INPUT_SERVER --room=$INPUT_ROOM_ID --file=$INPUT_FILE
+/matrix.sh --token=$INPUT_ACCESS_TOKEN --homeserver=$INPUT_SERVER --room=$INPUT_ROOM_ID --file=$INPUT_FILE $FILENAME
